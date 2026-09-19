0c18b86df4fcc1a8207cdd7d66e7bdfde7bb6232
Mateus Vargem <mateusvargem1999@gmail.com>
2026-09-19 15:35:43 -0300

    Category

--- src/main/java/com/coolproject/webservice/config/TestConfig.java
+++ src/main/java/com/coolproject/webservice/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.coolproject.webservice.entities.Category;
 import com.coolproject.webservice.entities.Order;
 import com.coolproject.webservice.entities.User;
 import com.coolproject.webservice.entities.enums.OrderStatus;
+import com.coolproject.webservice.repositories.CategoryRepository;
 import com.coolproject.webservice.repositories.OrderRepository;
 import com.coolproject.webservice.repositories.UserRepository;
 
@@ -23,9 +25,19 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
---
+++ src/main/java/com/coolproject/webservice/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.coolproject.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coolproject.webservice.entities.Category;
+import com.coolproject.webservice.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
